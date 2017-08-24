fe1d076b55962bd385350e1778f7cdb3f7c571b5
andrewpiggy <andrewpiggy1987@gmail.com>
2017-08-24 16:21:09 -0700

    Add lightgbm baseline.

--- production/lightgbm_baseline.py
+++ production/lightgbm_baseline.py
@@ -1,12 +1,12 @@
-# XGBoost baseline for feature engineering. Highlights:
+# LightGBM baseline for feature engineering. Highlights:
 # 1. Encode missing data for object type and leave other types missing data for
-#    xgboost to handle it.
+#    lightgbm to handle it.
 # 2. Make 10% of training data as validating data.
 #
-# Training result: [189] train-mae:0.066996 valid-mae:0.065312
-# Public score: 0.0655300
+# Training result: [719] train's mae: 0.0675895 valid's mae: 0.0644252
+# Public score: 0.0647086
+import lightgbm as lgbm
 import pandas as pd
-import xgboost as xgb
 from sklearn.preprocessing import LabelEncoder
 
 TRAINING_DATA_FILE = "../../data/train_2016_v2.csv"
@@ -33,7 +33,7 @@ def encode_data():
 
 
 def get_model_train_valid_data():
-    print('Creating training and validation data for xgboost.')
+    print('Creating training and validation data for lightgbm.')
     train_properties_df =\
         train_df.merge(properties_df, how='left', on='parcelid')
     train_index = []
@@ -45,43 +45,50 @@ def get_model_train_valid_data():
             valid_index.append(i)
     train_dataset = train_properties_df.iloc[train_index]
     valid_dataset = train_properties_df.iloc[valid_index]
-    # create train and valid data for xgboost.
+    # create train and valid data for lightgbm.
     train_x = train_dataset.drop(
         ['parcelid', 'logerror', 'transactiondate'], axis=1)
     train_y = train_dataset['logerror'].values
     valid_x = valid_dataset.drop(
         ['parcelid', 'logerror', 'transactiondate'], axis=1)
     valid_y = valid_dataset['logerror'].values
-    return [xgb.DMatrix(train_x, label=train_y),
-            xgb.DMatrix(valid_x, label=valid_y),
+    return [lgbm.Dataset(train_x, label=train_y),
+            lgbm.Dataset(valid_x, label=valid_y),
             train_x.columns]
 
 
 def train_model():
     print('Training the model.')
     params = {
-        'eta': 0.033,
-        'max_depth': 6,
-        'subsample': 0.80,
-        'objective': 'reg:linear',
-        'eval_metric': 'mae',
-        'silent': 1
+        'max_bin': 10,
+        'learning_rate': 0.0021,
+        'boosting_type': 'gbdt',
+        'objective': 'regression',
+        'metric': 'mae',
+        'sub_feature': 0.5,
+        'bagging_fraction': 0.85,
+        'bagging_freq': 40,
+        'num_leaves': 512,
+        'min_data': 500,
+        'min_hessian': 0.05,
+        'verbose': 0
     }
-    watchlist = [(xgboost_train_df, 'train'), (xgboost_valid_df, 'valid')]
-    return xgb.train(params, xgboost_train_df, 10000, watchlist,
-                     early_stopping_rounds=100, verbose_eval=10)
+    return lgbm.train(params, lgbm_train_df, num_boost_round=1000,
+                      valid_sets=[lgbm_train_df, lgbm_valid_df],
+                      valid_names=['train', 'valid'],
+                      early_stopping_rounds=100, verbose_eval=10)
 
 
 def get_model_test_data():
     print('Building test set.')
     test_df['parcelid'] = test_df['ParcelId']
     test_properties_df = test_df.merge(properties_df, how='left', on='parcelid')
-    return xgb.DMatrix(test_properties_df[train_columns])
+    return test_properties_df[train_columns]
 
 
 def predict_test():
     print('Predicting on test data.')
-    test_predict = xgboost_model.predict(xgboost_test_df)
+    test_predict = lgbm_model.predict(lgbm_test_df)
     tmp_df = pd.read_csv(TEST_DATA_FILE)
     for column in tmp_df.columns[tmp_df.columns != 'ParcelId']:
         tmp_df[column] = test_predict
@@ -97,9 +104,8 @@ def write_result():
 if __name__ == "__main__":
     train_df, properties_df, test_df = read_data()
     encode_data()
-    xgboost_train_df, xgboost_valid_df, train_columns =\
-        get_model_train_valid_data()
-    xgboost_model = train_model()
-    xgboost_test_df = get_model_test_data()
+    lgbm_train_df, lgbm_valid_df, train_columns = get_model_train_valid_data()
+    lgbm_model = train_model()
+    lgbm_test_df = get_model_test_data()
     result_df = predict_test()
     write_result()
